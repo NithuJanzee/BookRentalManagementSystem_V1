07ff2606b7b9ca6b17d7538e6e8254e295cc298d
Nithushan J. <UT01268@unicomtic.com>
2024-10-11 13:52:12 +0530

    PrivateFildCreated

---
+++ BookRentalManagementSystem_V1/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    internal class Book
+    {
+        private string BookId;
+        private string Title;
+        private string Author;
+        private decimal RentalPrice;
+
+        public Book(string bookID , string title , string author , decimal rentalPrice)
+        {
+            BookId = bookID;
+            Title = title;
+            Author = author;
+            RentalPrice = rentalPrice;
+        }
+    }
+}
